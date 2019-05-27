68018a5702d0f2b9610ca0d77a329d45def231a8
Utkarshsteve <33666537+Utkarshsteve@users.noreply.github.com>
2019-05-27 14:52:30 +0530

    Update 5.c

--- 5/5.c
+++ 5/5.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<ctype.h>
 #include<stdlib.h>
-char res[5],arg1[5],op[2],arg2[5];
+char res[5],arg1[5],op[2],arg2[5],c=0;
 void main()
 {
         FILE *fp1,*fp2;
         fp1=fopen("input.txt","r");
         fp2=fopen("output.txt","w");
-        while(!feof(fp1))
+        while(!feof(fp1)&&c<4)
         {
                 fscanf(fp1,"%s%s%s%s",res,arg1,op,arg2);
                 if(strcmp(op,"+")==0)
@@ -42,6 +42,7 @@ void main()
                         fprintf(fp2,"\nLD R0,%s",arg1);
                         fprintf(fp2,"\nST %s,R0",res);
                 }
+                c++;
          }
          fclose(fp1);
          fclose(fp2);
